ef742201e223c32315a6613dd53a3929b9f41227
Jagapathi Reddy <jagapathir@taknevit.com>
2021-07-13 14:55:33 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hey Welcome'));
+app.get('/', (req, res) => res.send('Hey! It's finished'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
